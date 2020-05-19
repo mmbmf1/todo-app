9c00ec9835529dc99dbe7554879f3262066fb1ff
Michael <mmbmf.mm@gmail.com>
2020-05-19 08:14:43 -0500

    new list title

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -23,7 +23,7 @@ function InputTodo() {
 
   return (
     <Fragment>
-      <h1 className="text-center mt-5">PERN Todo List</h1>
+      <h1 className="text-center mt-5">Todo List</h1>
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
